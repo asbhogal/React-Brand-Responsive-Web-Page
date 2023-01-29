41ae8c5f8b6a67af3e01c61d5771531adffb583c
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-29 15:29:32 +0000

    added semantic scaffolding

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Container from "./Container";
+import Footer from "./Footer";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Container />
+            <Footer />
+        </>
+    )
+};
+
+export default App;
---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+const Container = () => {
+    return (
+        <main>
+            <section className="HeroSection"></section>
+            <section className="SecondSection"></section>
+            <section className="TestimonialsSection"></section>
+            <section className="CTASection"></section>
+        </main>
+    )
+};
+
+export default Container;
