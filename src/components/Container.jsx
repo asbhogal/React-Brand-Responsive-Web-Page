c597e2109b155eed784a752a3b2c559bfcff9b2f
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-29 19:18:16 +0000

    further content populated and positioning changes

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,7 +4,31 @@ const Container = () => {
             <section className="HeroSection">
                 <article className="LeftContent">Left Content</article>
                 <article className="RightContent">Right Content</article>
-                <article className="SummaryContent">Bottom Content</article>
+                <article className="SummaryContent">
+                    <ul className="SummaryContentList">
+                        <li>
+                            <section>
+                                <span className="FeaturePoint"></span>
+                                <h3>Feature one</h3>
+                            </section>
+                            <p>All base UI elements are made using Nested Symbols and shared styles that are logically connected with one another.</p>
+                        </li>
+                        <li>
+                            <section>
+                                <span className="FeaturePoint"></span>
+                                <h3>Feature two</h3>
+                            </section>
+                            <p>All base UI elements are made using Nested Symbols and shared styles that are logically connected with one another.</p>
+                        </li>
+                        <li>
+                            <section>
+                               <span className="FeaturePoint"></span>
+                               <h3>Feature three</h3>
+                            </section>
+                            <p>All base UI elements are made using Nested Symbols and shared styles that are logically connected with one another.</p>
+                        </li>
+                    </ul>
+                </article>
             </section>
             <section className="SecondSection">
                 <article className="LeftContent"></article>
