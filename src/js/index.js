26df6378bcb0030c7a6e4df60f4dc882eb51b41e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-29 15:21:18 +0000

    added import directives for stylesheets and index.js content

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react";
+import App from "../components/App";
+
+const   rootElement = document.getElementById('root'),
+        root = ReactDOM.createRoot(rootElement);
+
+root.render (
+    <App />
+)
