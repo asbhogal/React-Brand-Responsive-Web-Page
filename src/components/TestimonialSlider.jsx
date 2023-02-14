9afba7789173e6f9c777ef21847c18fee058dc3b
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-14 12:36:20 +0000

    ran build prod for deployment - added FreeMode module to TestimonialSlider

--- src/components/TestimonialSlider.jsx
+++ src/components/TestimonialSlider.jsx
@@ -1,17 +1,21 @@
-import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination, A11y } from 'swiper';
+import { Autoplay, FreeMode, Pagination, A11y } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 import data from "../js/data";
 
 const TestimonialSlider = () => {
     return (
         <Swiper
-            modules={[Autoplay, Pagination, A11y]}
+            modules={[Autoplay, FreeMode, Pagination, A11y]}
+            spaceBetween={ 50 }
+            pagination={{ clickable: true }}
+            autoplay={ true }
+            loop={ true }
+            freeMode={ true }
+
             breakpoints = {{
                 1440: {
                     slidesPerView: 3
@@ -27,29 +31,24 @@ const TestimonialSlider = () => {
                     slidesPerView: 1
                 }
             }}
-            spaceBetween={ 50 }
-            pagination={{ clickable: true }}
-            autoplay={ true }
-            loop={ true }
-            onSwiper={ (swiper) => console.log(swiper) }
-            onSlideChange={() => console.log('slide change')}
         >
-            { data.map(user => (
-    <SwiperSlide className="TestimonialCard" key={ user.id }>
-        <div>
-            <p className="TestimonialQuote">{ user.description }</p>
-            <img className="TestimonialImage" src={ require(`/src/assets/images/Testimonial-Image-${ user.img }`) }></img>
-            <p className="TestimonialName">{ user.name }</p>
-            <div className="TestimonialBottomSection">
-                <p className="TestimonialTitle">{ user.title }</p>
-                <div className="SocialIcons">
-                    <img src={ require(`/src/assets/logos/${ user.socialIcons.facebookIcon }`) }></img>
-                    <img src={ require(`/src/assets/logos/${ user.socialIcons.twitterIcon }`) }></img>
+
+        { data.map(user => (
+            <SwiperSlide className="TestimonialCard" key={ user.id }>
+                <div>
+                    <p className="TestimonialQuote">{ user.description }</p>
+                    <img className="TestimonialImage" src={ require(`/src/assets/images/Testimonial-Image-${ user.img }`) }></img>
+                    <p className="TestimonialName">{ user.name }</p>
+                    <div className="TestimonialBottomSection">
+                        <p className="TestimonialTitle">{ user.title }</p>
+                        <div className="SocialIcons">
+                            <img src={ require(`/src/assets/logos/${ user.socialIcons.facebookIcon }`) }></img>
+                            <img src={ require(`/src/assets/logos/${ user.socialIcons.twitterIcon }`) }></img>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </SwiperSlide>
-    )) }
+            </SwiperSlide>
+        )) }
         </Swiper>
     );
   };
